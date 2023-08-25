cec714cc462eb43fa161a3807bfd4a7a0cb1899d
phenix3443 <phenix3443@gmail.com>
2023-08-25 15:28:22 +0800

    feat: add mythril (#2)

--- init.sh
+++ init.sh
@@ -7,6 +7,4 @@ solc-select install 0.8.18
 solc-select use 0.8.18
 
 #mythril
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup default nightly
-pip3 install mythril
+docker pull mythril/myth
