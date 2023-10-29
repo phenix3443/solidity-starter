382676e959db09e13404c2b00d8253a583b599cd
phenix3443 <phenix3443@gmail.com>
2023-10-29 12:13:30 +0800

    feat: test receive function

--- src/Hello.sol
+++ src/Hello.sol
@@ -5,20 +5,9 @@ import "forge-std/Script.sol"; // solhint-disable
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Hello {
-    struct Person {
-        string name;
-        uint256 age;
-    }
+    event Receive(address indexed account, uint256 indexed amount);
 
-    Person[] public peoples;
-
-    constructor(string memory name, uint256 age) {
-        Person memory p = Person(name, age);
-        peoples.push(p);
-    }
-
-    function get() public view returns (Person[] memory) {
-        Person[] memory newPeoples = peoples;
-        return newPeoples;
+    receive() external payable {
+        emit Receive(msg.sender, msg.value);
     }
 }
