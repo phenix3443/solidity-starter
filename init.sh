042687ac7134dbb3f028132ecba06e255ac44ce3
phenix3443 <phenix3443@gmail.com>
2024-01-26 23:14:45 +0800

    feat(script): change init script (#5)
    feat: update init script

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# foundry
+curl -L https://foundry.paradigm.xyz | bash && foundryup
 # slither
-pip3 install slither-analyzer
-pip3 install solc-select
-solc-select install 0.8.20
-solc-select use 0.8.20
-
+pip3 install slither-analyzer &&
+    pip3 install solc-select &&
+    solc-select install 0.8.20 &&
+    solc-select use 0.8.20
 #mythril
 docker pull mythril/myth
